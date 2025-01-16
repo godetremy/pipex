a28458cf826e1ec961982c3a681ae3952e08b3ff
rgodet <marvin@student.42.fr>
2025-01-16 09:37:56 +0100

    fix: Removing unused temp file

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: rgodet <rgodet@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 09:05:29 by rgodet            #+#    #+#             */
-/*   Updated: 2025/01/15 16:20:27 by rgodet           ###   ########.fr       */
+/*   Updated: 2025/01/16 09:37:40 by rgodet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,6 @@ int	main(int argc, char **argv, char **envp)
 	params.cmd1 = parse_command(params.cmd1_str, envp);
 	params.cmd2 = parse_command(params.cmd2_str, envp);
 	ft_execute_cmd(params, envp, &status);
-	unlink("/tmp/pipex_temp");
 	free_cmd(params.cmd1);
 	free_cmd(params.cmd2);
 	return (status);
